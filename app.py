92c45e5bcf9e0ddbc80e5dd5af02692794d80b96
yghor <yghorcastello.backend@gmail.com>
2022-10-16 20:08:32 -0300

    Alteração Chave Estrangeira AddCarros

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask import Flask, current_app, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
 from flask_migrate import Migrate
-# from forms import FormCadastrarCliente, FormCadastrarCarros
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -19,7 +17,6 @@ migrate = Migrate(app, db)
 
 
 class Pessoa(db.Model):
-    __tablename__ = "pessoa"
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String, nullable=False)
     cpf = db.Column(db.String, nullable=False, unique=True)
@@ -31,34 +28,25 @@ class Pessoa(db.Model):
         self.cpf = cpf
         self.endereco = endereco
 
-
 class Carro(db.Model):
-    __tablename__ = "carro"
     id = db.Column(db.Integer, primary_key=True)
     carro = db.Column(db.String, nullable=False)
     modelo = db.Column(db.String, nullable=False)
     cor = db.Column(db.String, nullable=False)
     pessoa_id = db.Column(db.Integer, db.ForeignKey('pessoa.id'), nullable=False)
 
+    pessoa = db.relationship('Pessoa', foreign_keys=pessoa_id)
 
-    def __init__(self, pessoa_id, carro, modelo, cor):
-        self.pessoa_id = pessoa_id
+    def __init__(self, carro, modelo, cor, pessoa_id):
         self.carro = carro
         self.modelo = modelo
         self.cor = cor
+        self.pessoa_id = pessoa_id
 
 @app.route("/")
 def home():
-    # form_cliente = FormCadastrarCliente
-    # form_carro = FormCadastrarCarros
-    # if form_cliente.validate_on_submit():
-    #     #fez o cadastro com sucesso
-    #
-    # if form_carro.validate_on_submit():
-    #     # fez o cadastro com sucesso
     pessoa = Pessoa.query.all()
     carro = Carro.query.all()
-    # return render_template("base.html", form_cliente=form_cliente, form_carro=form_carro)
     return render_template("base.html", pessoa=pessoa, carro=carro)
 
 
@@ -79,31 +67,31 @@ def addcliente():
         db.session.add(nova_pessoa)
         db.session.commit()
         return redirect(url_for('home'))
-
     return render_template("addcliente.html")
 
 #CADASTRANDO CARROS
 @app.route("/addcarro", methods=["GET", "POST"])
 def addcarro():
+    pessoa = Pessoa.query.all()
     if request.method == "POST":
-        pessoa_id = Carro(request.form['pessoa_id'])
-        novo_carro = Carro(request.form['carro'], request.form['modelo'], request.form['cor'])
-        db.session.add(pessoa_id, novo_carro)
+        novo_carro = Carro(request.form['carro'], request.form['modelo'], request.form['cor'], request.form['pessoa_id'])
+        db.session.add(novo_carro)
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template("addcarro.html")
+    return render_template("addcarro.html", pessoa=pessoa)
 
 #UPDATE - ATUALIZAR CLIENTES
 @app.route("/update/<int:pessoa_id>", methods=["GET", "POST"])
 def update(pessoa_id):
     pessoa = Pessoa.query.get(pessoa_id)
+    carro = Carro.query.get(pessoa_id)
     if request.method == "POST":
         pessoa.nome = request.form['nome']
         pessoa.cpf = request.form['cpf']
         pessoa.endereco = request.form['endereco']
-        pessoa.carro = request.form['carro']
-        pessoa.modelo = request.form['modelo']
-        pessoa.cor = request.form['cor']
+        carro.carro = request.form['carro']
+        carro.modelo = request.form['modelo']
+        carro.cor = request.form['cor']
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("update.html", pessoa=pessoa)
@@ -118,5 +106,5 @@ def delete(pessoa_id):
 
 with app.app_context():
     if __name__ == "__main__":
+        app.run(debug=True)
         db.create_all()
-        app.run(debug=True) #responsavel por atualizar o site em toda modificação
