2bfec2e50092bc2fbe3fe9b505b19c22d090cbd2
yghor <yghorcastello.backend@gmail.com>
2022-10-16 15:45:25 -0300

    Atualização do BD

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, current_app, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 # from forms import FormCadastrarCliente, FormCadastrarCarros
+
 
 
 app = Flask(__name__)
@@ -19,10 +20,10 @@ migrate = Migrate(app, db)
 
 class Pessoa(db.Model):
     __tablename__ = "pessoa"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String(100))
-    cpf = db.Column(db.String(11))
-    endereco = db.Column(db.String(150))
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String, nullable=False)
+    cpf = db.Column(db.String, nullable=False, unique=True)
+    endereco = db.Column(db.String, nullable=False)
     carros = db.relationship('Carro', backref='proprietario', lazy=True)
 
     def __init__(self, nome, cpf, endereco):
@@ -33,13 +34,12 @@ class Pessoa(db.Model):
 
 class Carro(db.Model):
     __tablename__ = "carro"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    carro = db.Column(db.String(30))
-    modelo = db.Column(db.String(30))
-    cor = db.Column(db.String(30))
-    pessoa_id = db.Column(db.Integer, db.ForeignKey('pessoa.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    carro = db.Column(db.String, nullable=False)
+    modelo = db.Column(db.String, nullable=False)
+    cor = db.Column(db.String, nullable=False)
+    pessoa_id = db.Column(db.Integer, db.ForeignKey('pessoa.id'), nullable=False)
 
-    pessoa = db.relationship("Pessoa", foreign_keys=pessoa_id)
 
     def __init__(self, pessoa_id, carro, modelo, cor):
         self.pessoa_id = pessoa_id
@@ -120,5 +120,3 @@ with app.app_context():
     if __name__ == "__main__":
         db.create_all()
         app.run(debug=True) #responsavel por atualizar o site em toda modificação
-
-
